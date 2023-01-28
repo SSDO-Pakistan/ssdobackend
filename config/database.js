4a0cc4d7083ff4b92413f108c2d493030de10157
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-01-28 14:53:48 +0500

    changes in db

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "ssdobackendnew"),
+      host: env("DATABASE_HOST", "containers-us-west-195.railway.app"),
+      port: env.int("DATABASE_PORT", 6943),
+      database: env("DATABASE_NAME", "railway"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "postgres"),
+      password: env("DATABASE_PASSWORD", "H5ufd625PTRbG15w4Rcj"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
