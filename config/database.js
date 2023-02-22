89c3316a11b0b2a8ac131ba070fbe401fcaa007e
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-02-23 00:00:49 +0500

    change password according to the railway app

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       port: env.int("DATABASE_PORT", 6943),
       database: env("DATABASE_NAME", "railway"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "H5ufd625PTRbG15w4Rcj"),
+      password: env("DATABASE_PASSWORD", "OW9Sw52MYJQl9z93el9V"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
