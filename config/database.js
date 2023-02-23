b6e6efed1b1900f61dfee58f8da14f4297bffe64
Amjad Iqbal Khan <amjad.iqbal@VIBD01.local>
2023-02-23 16:14:01 +0500

    update the database settings

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "containers-us-west-195.railway.app"),
-      port: env.int("DATABASE_PORT", 6943),
+      host: env("DATABASE_HOST", "containers-us-west-136.railway.app"),
+      port: env.int("DATABASE_PORT", 5809),
       database: env("DATABASE_NAME", "railway"),
       user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "OW9Sw52MYJQl9z93el9V"),
