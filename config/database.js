1c92869bdec63e5e5a5e88554c0c02188822a060
Amjad Iqbal Khan <amjad.iqbal@VIBD01.local>
2023-03-01 10:32:21 +0500

    change db credetials

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "containers-us-west-136.railway.app"),
-      port: env.int("DATABASE_PORT", 5809),
+      host: env("DATABASE_HOST", "containers-us-west-195.railway.app"),
+      port: env.int("DATABASE_PORT", 6943),
       database: env("DATABASE_NAME", "railway"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "OW9Sw52MYJQl9z93el9V"),
+      password: env("DATABASE_PASSWORD", "5Jr2NRZyayj427EjNH5s"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
