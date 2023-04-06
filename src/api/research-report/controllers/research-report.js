ed66f7ac386e5cacfdf5cc20601ad52cc36cef86
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-04-06 23:58:39 +0500

    Added Research  Reports

---
+++ src/api/research-report/controllers/research-report.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * research-report controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::research-report.research-report');
---
+++ src/api/research-report/routes/research-report.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * research-report router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::research-report.research-report');
