910cb3a9bb5fb6f1f8520b8f9f66e19517df0631
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2024-02-02 23:07:32 +0500

    Added new password to database.js file

--- config/database.js
+++ config/database.js
@@ -6,8 +6,10 @@ module.exports = ({ env }) => ({
       port: env.int("DATABASE_PORT", 5809),
       database: env("DATABASE_NAME", "railway"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "OW9Sw52MYJQl9z93el9V"),
+      password: env("DATABASE_PASSWORD", "4bc436FaC4gFFa4c2CGGca5C64DGB3cc"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
+
+////////////////
