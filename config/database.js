4b38c97927d70fb07ece77dd4e2c868e4d770b6a
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2024-02-03 00:28:41 +0500

    Changes in database file

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "containers-us-west-136.railway.app"),
-      port: env.int("DATABASE_PORT", 5809),
+      host: env("DATABASE_HOST", "viaduct.proxy.rlwy.net"),
+      port: env.int("DATABASE_PORT", 21731),
       database: env("DATABASE_NAME", "railway"),
       user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "4bc436FaC4gFFa4c2CGGca5C64DGB3cc"),
